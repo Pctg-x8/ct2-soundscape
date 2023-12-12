8209ef61bdec9d5bd9e24ee3c2597fe3929e3c21
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-12-12 12:16:41 +0900

    fix: object types

--- admin-console/remix.env.d.ts
+++ admin-console/remix.env.d.ts
@@ -2,6 +2,7 @@
 /// <reference types="@remix-run/cloudflare" />
 
 import "@remix-run/server-runtime";
+import type { D1Database, R2Bucket } from "@cloudflare/workers-types";
 
 declare module "__STATIC_CONTENT_MANIFEST" {
     const manifest: string;
