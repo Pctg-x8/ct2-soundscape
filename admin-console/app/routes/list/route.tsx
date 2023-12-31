2b6bb4fb28732e5f79feb15fe67bd251aa9ce829
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-12-31 15:51:19 +0900

    add: license updating

--- admin-console/app/routes/list/route.tsx
+++ admin-console/app/routes/list/route.tsx
@@ -74,6 +74,8 @@ export async function action({ request, context }: ActionFunctionArgs) {
             maxBPM: zfd.numeric(),
             comment: zfd.text(z.string().optional()).transform((x) => x ?? ""),
             time: zfd.text().transform((x) => new Date(x)),
+            licenseType: zfd.numeric(),
+            licenseText: zfd.text(z.string().optional()).transform((x) => x ?? ""),
             file: zfd.file(z.instanceof(File).optional()),
         })
     );
@@ -88,11 +90,30 @@ export async function action({ request, context }: ActionFunctionArgs) {
         if (input.fromEditDialog !== false) {
             // save required
 
+            let license: License.Type;
+            switch (input.licenseType) {
+                case License.PublicDomain:
+                case License.CreativeCommons4.BY:
+                case License.CreativeCommons4.BY_SA:
+                case License.CreativeCommons4.BY_NC:
+                case License.CreativeCommons4.BY_ND:
+                case License.CreativeCommons4.BY_NC_SA:
+                case License.CreativeCommons4.BY_NC_ND:
+                    license = input.licenseType;
+                    break;
+                case 999:
+                    license = input.licenseText;
+                    break;
+                default:
+                    throw new Error("invalid license input");
+            }
+
             const id = input.fromEditDialog;
             const newDetails: Partial<ContentDetails> = {
                 ...pick(input, "title", "artist", "genre", "comment"),
                 bpmRange: { min: input.minBPM, max: input.maxBPM },
                 dateJst: input.time,
+                license,
             };
 
             if (input.file instanceof File) {
@@ -164,6 +185,123 @@ export default function Page() {
     );
 }
 
+function EditForm({
+    defaultValues,
+    fetcher,
+}: {
+    readonly defaultValues: EntryTableRow;
+    readonly fetcher: FetcherWithComponents<unknown>;
+}) {
+    const [currentLicenseSelection, setCurrentLicenseSelection] = useState<number>(() => {
+        switch (defaultValues.license) {
+            case License.PublicDomain:
+            case License.CreativeCommons4.BY:
+            case License.CreativeCommons4.BY_NC:
+            case License.CreativeCommons4.BY_SA:
+            case License.CreativeCommons4.BY_ND:
+            case License.CreativeCommons4.BY_NC_ND:
+            case License.CreativeCommons4.BY_NC_SA:
+                return defaultValues.license;
+            default:
+                return 999;
+        }
+    });
+
+    return (
+        <fetcher.Form method="post" encType="multipart/form-data" className="contentForm">
+            <fieldset disabled={fetcher.state === "submitting"}>
+                <section>
+                    <label htmlFor="title">タイトル</label>
+                    <input id="title" name="title" defaultValue={defaultValues.title} required />
+                </section>
+                <section>
+                    <label htmlFor="artist">アーティスト表記名</label>
+                    <input id="artist" name="artist" defaultValue={defaultValues.artist} required />
+                </section>
+                <section>
+                    <label htmlFor="genre">ジャンル</label>
+                    <input id="genre" name="genre" defaultValue={defaultValues.genre} required />
+                </section>
+                <section>
+                    <p className="labelLike">BPM範囲</p>
+                    <section className="inputGroup">
+                        <input
+                            name="minBPM"
+                            type="number"
+                            defaultValue={defaultValues.bpmRange.min}
+                            min={1}
+                            className="bpmInputBox"
+                            required
+                        />
+                        〜
+                        <input
+                            name="maxBPM"
+                            type="number"
+                            defaultValue={defaultValues.bpmRange.max}
+                            min={1}
+                            className="bpmInputBox"
+                            required
+                        />
+                    </section>
+                </section>
+                <section>
+                    <label htmlFor="time">制作日</label>
+                    <input
+                        id="time"
+                        name="time"
+                        type="date"
+                        defaultValue={`${defaultValues.year.toString().padStart(4, "0")}-${defaultValues.month
+                            .toString()
+                            .padStart(2, "0")}-${defaultValues.day.toString().padStart(2, "0")}`}
+                        required
+                    />
+                </section>
+                <section>
+                    <label htmlFor="comment">コメント（Markdown可）</label>
+                    <textarea id="comment" name="comment" rows={1} defaultValue={defaultValues.comment} />
+                </section>
+                <section>
+                    <label htmlFor="file">ファイル（置き換える場合）</label>
+                    <input id="file" name="file" type="file" accept="audio/*" />
+                </section>
+                <section>
+                    <label htmlFor="licenseType">ライセンス形態</label>
+                    <div className="licenseInputs">
+                        <select
+                            id="licenseType"
+                            name="licenseType"
+                            defaultValue={currentLicenseSelection}
+                            onChange={(e) => setCurrentLicenseSelection(Number(e.currentTarget.value))}
+                        >
+                            <option value={License.PublicDomain}>CC0</option>
+                            <option value={License.CreativeCommons4.BY}>CC-BY</option>
+                            <option value={License.CreativeCommons4.BY_SA}>CC-BY-SA</option>
+                            <option value={License.CreativeCommons4.BY_NC}>CC-BY-NC</option>
+                            <option value={License.CreativeCommons4.BY_NC_SA}>CC-BY-NC-SA</option>
+                            <option value={License.CreativeCommons4.BY_NC_ND}>CC-BY-NC-ND</option>
+                            <option value={License.CreativeCommons4.BY_ND}>CC-BY-ND</option>
+                            <option value={999}>その他</option>
+                        </select>
+                        <input
+                            name="licenseText"
+                            defaultValue={typeof defaultValues.license === "string" ? defaultValues.license : ""}
+                            disabled={currentLicenseSelection !== 999}
+                        />
+                    </div>
+                </section>
+                <section className="buttons">
+                    <button type="submit" name="fromEditDialog" value={defaultValues.id.toString()}>
+                        確定
+                    </button>
+                    <button type="submit" name="fromEditDialog" value="false">
+                        取り消し
+                    </button>
+                </section>
+            </fieldset>
+        </fetcher.Form>
+    );
+}
+
 const EditDialog = forwardRef(function EditDialog(
     { editing, fetcher }: { readonly editing: EntryTableRow; readonly fetcher: FetcherWithComponents<unknown> },
     ref: ForwardedRef<HTMLDialogElement>
@@ -171,72 +309,7 @@ const EditDialog = forwardRef(function EditDialog(
     return (
         <dialog ref={ref}>
             <h1>Edit #{editing.id}</h1>
-            <fetcher.Form method="post" encType="multipart/form-data" className="contentForm">
-                <fieldset disabled={fetcher.state === "submitting"} key={editing.id}>
-                    <section>
-                        <label htmlFor="title">タイトル</label>
-                        <input id="title" name="title" defaultValue={editing.title} required />
-                    </section>
-                    <section>
-                        <label htmlFor="artist">アーティスト表記名</label>
-                        <input id="artist" name="artist" defaultValue={editing.artist} required />
-                    </section>
-                    <section>
-                        <label htmlFor="genre">ジャンル</label>
-                        <input id="genre" name="genre" defaultValue={editing.genre} required />
-                    </section>
-                    <section>
-                        <p className="labelLike">BPM範囲</p>
-                        <section className="inputGroup">
-                            <input
-                                name="minBPM"
-                                type="number"
-                                defaultValue={editing.bpmRange.min}
-                                min={1}
-                                className="bpmInputBox"
-                                required
-                            />
-                            〜
-                            <input
-                                name="maxBPM"
-                                type="number"
-                                defaultValue={editing.bpmRange.max}
-                                min={1}
-                                className="bpmInputBox"
-                                required
-                            />
-                        </section>
-                    </section>
-                    <section>
-                        <label htmlFor="time">制作日</label>
-                        <input
-                            id="time"
-                            name="time"
-                            type="date"
-                            defaultValue={`${editing.year.toString().padStart(4, "0")}-${editing.month
-                                .toString()
-                                .padStart(2, "0")}-${editing.day.toString().padStart(2, "0")}`}
-                            required
-                        />
-                    </section>
-                    <section>
-                        <label htmlFor="comment">コメント（Markdown可）</label>
-                        <textarea id="comment" name="comment" rows={1} defaultValue={editing.comment} />
-                    </section>
-                    <section>
-                        <label htmlFor="file">ファイル（置き換える場合）</label>
-                        <input id="file" name="file" type="file" accept="audio/*" />
-                    </section>
-                    <section className="buttons">
-                        <button type="submit" name="fromEditDialog" value={editing.id.toString()}>
-                            確定
-                        </button>
-                        <button type="submit" name="fromEditDialog" value="false">
-                            取り消し
-                        </button>
-                    </section>
-                </fieldset>
-            </fetcher.Form>
+            <EditForm fetcher={fetcher} defaultValues={editing} key={editing.id} />
         </dialog>
     );
 });
