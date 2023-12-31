08c6a44528d153342b742c0eae338d6667548c30
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-12-31 14:18:13 +0900

    fix: add headers in loader response

--- app/app/routes/content.$id.details.tsx
+++ app/app/routes/content.$id.details.tsx
@@ -3,18 +3,17 @@ import { ContentId } from "soundscape-shared/src/content";
 import type { Details } from "~/components/DetailsPane";
 import { pick } from "soundscape-shared/src/utils/typeImpl";
 
+const cacheLength = 60 * 60 * 24 * 30;
+
 export async function loader({ context, params }: LoaderFunctionArgs) {
     const result = await context.contentRepository.get(new ContentId.External(Number(params["id"])));
     if (!result) throw new Response("not found", { status: 404 });
 
-    return json({
-        ...pick(result, "title", "artist", "genre", "bpmRange", "comment", "license"),
-        // TODO: Download Link URL
-    } satisfies Details);
-}
-
-export function headers(): HeadersInit {
-    const cacheLength = 60 * 60 * 24 * 30;
-
-    return { "Cache-Control": `max-age=${cacheLength}; s-maxage=${cacheLength}` };
+    return json(
+        {
+            ...pick(result, "title", "artist", "genre", "bpmRange", "comment", "license"),
+            // TODO: Download Link URL
+        } satisfies Details,
+        { headers: { "Cache-Control": `max-age=${cacheLength}; s-maxage=${cacheLength}` } }
+    );
 }
