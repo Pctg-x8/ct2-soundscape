3e2395b2862cd8218f9b37c4d38df9bc08254c2d
S.Percentage <Syn.Tri.Naga@gmail.com>
2023-12-31 16:01:25 +0900

    fix: mp3 extension

--- app/app/routes/content.$id.download.tsx
+++ app/app/routes/content.$id.download.tsx
@@ -3,12 +3,22 @@ import { ContentId } from "soundscape-shared/src/content";
 import Mime from "mime";
 import { _let } from "soundscape-shared/src/utils";
 
+function getExtension(contentType: string): string | null {
+    // Note: mimeだとaudio/mpegがmpgaとかいう謎の拡張子になるので例外対応する
+    const knownExtensionTable: Record<string, string> = {
+        "audio/mpeg": "mp3",
+    };
+    if (contentType in knownExtensionTable) return knownExtensionTable[contentType];
+
+    return Mime.getExtension(contentType);
+}
+
 export async function loader({ context, params }: LoaderFunctionArgs) {
     const id = new ContentId.External(Number(params["id"]));
     const info = await context.contentRepository.download(id);
     if (!info) throw new Response("not found", { status: 404 });
 
-    const suffix = _let(Mime.getExtension(info.contentType), (x) => (x === null ? "" : `.${x}`));
+    const suffix = _let(getExtension(info.contentType), (x) => (x === null ? "" : `.${x}`));
     const filename = `${params["id"]} - ${info.artist} - ${info.title}${suffix}`;
 
     // @ts-ignore
