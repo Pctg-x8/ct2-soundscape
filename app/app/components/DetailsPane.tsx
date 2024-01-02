6683aba9e9fb9b432b4f4d2228b1ed2e81dd1c0e
S.Percentage <Syn.Tri.Naga@gmail.com>
2024-01-02 12:40:51 +0900

    fix: pre style

--- app/app/components/DetailsPane.tsx
+++ app/app/components/DetailsPane.tsx
@@ -45,7 +45,7 @@ export default function DetailsPane({
 
 const Licenses = {
     googleMaterialIcons: `
-    Apache License
+                    Apache License
                Version 2.0, January 2004
             http://www.apache.org/licenses/
 
