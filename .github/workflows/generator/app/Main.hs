4d92db55c7dbdb2f7c1d547268d8002a3238468f
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2026-02-15 14:11:33 +0900

    rm: unused parts

--- .github/workflows/generator/app/Main.hs
+++ .github/workflows/generator/app/Main.hs
@@ -8,9 +8,7 @@ import System.Environment (getArgs)
 import System.FilePath ((</>))
 import Workflow.GitHub.Actions qualified as GHA
 import Workflow.GitHub.Actions.Predefined.Checkout qualified as Checkout
-import Workflow.GitHub.Actions.Predefined.Rust.Toolchain qualified as Rust
 import Workflow.GitHub.Actions.Predefined.SetupNode qualified as SetupNode
-import Workflow.GitHub.Actions.Predefined.SetupPNPM qualified as SetupPNPM
 
 secretCloudflareApiToken :: String
 secretCloudflareApiToken = GHA.mkExpression "secrets.CLOUDFLARE_API_TOKEN"
@@ -18,38 +16,9 @@ secretCloudflareApiToken = GHA.mkExpression "secrets.CLOUDFLARE_API_TOKEN"
 secretCloudflareAccountID :: String
 secretCloudflareAccountID = GHA.mkExpression "secrets.CLOUDFLARE_ACCOUNT_ID"
 
-headlessAdminConsoleProjectName :: String
-headlessAdminConsoleProjectName = "headless-admin-console"
-
-adminConsoleProjectName :: String
-adminConsoleProjectName = "ct2-soundscape-admin-console"
-
-appProjectName :: String
-appProjectName = "app"
-
 withDeploymentEnvironments :: GHA.Step -> GHA.Step
 withDeploymentEnvironments =
   GHA.env "CLOUDFLARE_ACCOUNT_ID" secretCloudflareAccountID . GHA.env "CLOUDFLARE_API_TOKEN" secretCloudflareApiToken
-
-filterProject :: String -> SetupPNPM.RunInstallOption -> SetupPNPM.RunInstallOption
-filterProject name opt = opt {SetupPNPM.runInstallArgs = SetupPNPM.runInstallArgs opt ++ ["-F", name]}
-
-useFrozenLockfile :: SetupPNPM.RunInstallOption -> SetupPNPM.RunInstallOption
-useFrozenLockfile opt = opt {SetupPNPM.runInstallArgs = SetupPNPM.runInstallArgs opt ++ ["--frozen-lockfile"]}
-
-headlessAdminConsoleDeploymentJob :: GHA.Job
-headlessAdminConsoleDeploymentJob =
-  GHA.runInEnvironment (GHA.RepositoryEnvironment "prod:headless-admin-console") $
-    GHA.namedAs "Deployment(Headless Admin Console)" $
-      GHA.job
-        [ GHA.namedAs "Checking out" $ Checkout.step Nothing,
-          GHA.namedAs "Setup PNPM" $
-            SetupPNPM.step
-              [ SetupPNPM.runInstallOption & filterProject "shared" & filterProject headlessAdminConsoleProjectName & useFrozenLockfile
-              ],
-          GHA.namedAs "Setup Rust" $ Rust.step & Rust.useStable & Rust.forTarget "wasm32-unknown-unknown",
-          GHA.namedAs "deploy" $ GHA.runStep "pnpm run deploy" & withDeploymentEnvironments & GHA.workAt "headless-admin-console"
-        ]
 
 adminConsoleDeploymentJob :: GHA.Job
 adminConsoleDeploymentJob =
