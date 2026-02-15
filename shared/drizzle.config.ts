58532b3a9a7a2e383ba07af5e96e3e6c93bb0cdc
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2026-02-15 18:32:39 +0900

    fix: update drizzle migrations meta

--- shared/drizzle.config.ts
+++ shared/drizzle.config.ts
@@ -1,11 +1,10 @@
 import { Config } from "drizzle-kit";
+import DBCredentials from "./db_credentials";
 
 export default {
+    dialect: "sqlite",
     schema: "./src/schema.ts",
     out: "./migrations/",
-    driver: "d1",
-    dbCredentials: {
-        wranglerConfigPath: "./wrangler.toml",
-        dbName: "soundscape",
-    },
+    driver: "d1-http",
+    dbCredentials: DBCredentials,
 } satisfies Config;
